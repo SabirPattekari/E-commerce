fa05e366121552fdde1bafa10fd9d275b8a0651d
sabir <sabir14032002@gmail.com>
2024-10-30 22:44:44 +0800

    Add all project files

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -7,18 +7,16 @@ const Home = () => {
         <div className="card bg-dark text-white border-0 mx-3">
           <img
             className="card-img img-fluid"
-            src="main.png.jpg"
+            src="main.avif"
             alt="Card"
             height={500}
           />
           <div className="card-img-overlay d-flex align-items-center">
             <div className="container">
-              <h5 className="card-title fs-1 text fw-lighter">New Season Arrivals</h5>
+              {/* <h5 className="card-title fs-1 text fw-lighter">Best Deals One Step Ahead</h5>
               <p className="card-text fs-5 d-none d-sm-block ">
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
-              </p>
+              "Celebrate Diwali with Big Savings: Light Up Your Festivities with Our Exclusive Deals!"
+              </p> */}
             </div>
           </div>
         </div>
